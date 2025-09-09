a35de2ef5d97f5d15e450741517ca476272abd48
tedcodesm <kaahenjoroge@gmail.com>
2025-09-09 21:47:30 +0300

    li

--- lib/db.ts
+++ lib/db.ts
@@ -3,7 +3,7 @@ console.log(process.env.NEXT_PUBLIC_MONGO_URI);
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect("mongodb+srv://kaahenjoroge_db_user:se3ox3GmdbsAcl38@cluster0.s8gr7sq.mongodb.net/Nextjsusers?retryWrites=true&w=majority&appName=Cluster0");
+    const conn = await mongoose.connect(process.env.NEXT_PUBLIC_MONGO_URI);
 
     console.log(` MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
