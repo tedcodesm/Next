4b1d80457e89ef7a22deb40b0aba96013e023dfa
tedcodesm <kaahenjoroge@gmail.com>
2025-09-09 21:45:34 +0300

    error

--- app/user/page.tsx
+++ app/user/page.tsx
@@ -1,9 +1,39 @@
-import React from 'react'
+"use client"
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
 
-const page = () => {
-  return (
-    <div>user page</div>
-  )
+const UserPage = () => {
+  const [user , setUser] = useState([]);
+
+  const GetUser = async () => {
+   try {
+     const response = await axios.get('/api/users');
+    setUser(response.data);
+   } catch (error) {
+        console.error("Error fetching users:", error);
+
+    
+   }
+  }
+  useEffect(() => {
+    GetUser();
+  }, [])
+  console.log(user);
+
+
+ return (
+  <div className='flex min-h-screen items-center justify-center flex-col gap-2'>
+    <h1 className="text-xl font-bold">Users</h1>
+    {user.length > 0 ? (
+      user.map((u, index) => (
+        <p key={index}>{u.name} - {u.email}</p>
+      ))
+    ) : (
+      <p>No users found</p>
+    )}
+  </div>
+);
+
 }
 
-export default page
+export default UserPage
